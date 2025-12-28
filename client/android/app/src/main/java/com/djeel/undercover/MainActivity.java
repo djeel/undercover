1acdc2f75e2c1ea0cf700253d7c709b27f9db50a
djeel <49989313+djeel@users.noreply.github.com>
2025-12-28 22:40:47 +0100

    feat: Add Android platform to Capacitor project.

---
+++ client/android/app/src/main/java/com/djeel/undercover/MainActivity.java
@@ -0,0 +1,5 @@
+package com.djeel.undercover;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.djeel.undercover',
+  appName: 'Undercover',
+  webDir: 'dist'
+};
+
+export default config;
